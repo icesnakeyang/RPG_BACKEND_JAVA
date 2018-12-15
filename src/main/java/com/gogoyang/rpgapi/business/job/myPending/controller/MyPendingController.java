cc37e8a7b8181d0ce788e930fa50fe956d4c98d2
ly <000000>
2018-12-15 09:04:22 +0800

    ok

---
+++ src/main/java/com/gogoyang/rpgapi/business/job/myPending/controller/MyPendingController.java
@@ -0,0 +1,12 @@
+package com.gogoyang.rpgapi.business.job.myPending.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 我发布的任务，但没有成交的
+ */
+@RestController
+@RequestMapping("/mypending")
+public class MyPendingController {
+}
